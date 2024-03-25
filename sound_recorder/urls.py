8fa45cd1cf0881b4b702e080af6aa1a2a54449bb
Juliusz <01169303@pw.edu.pl>
2024-03-25 22:54:29 +0100

    final commit

--- sound_recorder/urls.py
+++ sound_recorder/urls.py
@@ -4,6 +4,8 @@ from .views import upload_recording
 from .views import delete_recording
 from .views import rename_recording
 from .views import edit_recording_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.record, name='record'),
@@ -14,4 +16,4 @@ urlpatterns = [
     path('rename/<int:recording_id>/', rename_recording, name='rename_recording'),
     path('edit/<int:recording_id>/', edit_recording_view, name='edit_recording'),
     path('record-camera/', views.record_camera, name='record_camera'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
